5b2882207a898f0f1d8cdd047272104703a8e7ed
Night1611 <night@loli.gs>
2020-08-16 11:52:12 +0800

    完善了车票购买界面

--- TTS_Server/MainWindow.xaml.cs
+++ TTS_Server/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,42 @@ namespace TTS_Server {
 		public MainWindow() {
 			InitializeComponent();
 		}
+
+		/*
+		public AllTrainAndStationInfo trainAndStationInfos;
+
+		public struct TrainAndStationInfo {
+			public int TrainNumber { get; set; } //列车编号
+			public int StationNumber { get; set; } //车站编号
+			public DateTime ArriveTime { get; set; } //到站时间
+			public DateTime LeaveTime { get; set; } //出站时间
+			public int RemainSeats { get; set; } //剩余座位
+		}
+
+		public class AllTrainAndStationInfo : ObservableCollection<TrainAndStationInfo> { } //定义集合
+
+		private AllTrainAndStationInfo SubmitTicketQuery(ClientWindow.TicketQueryInfo ticketQueryInfo) {
+			//发送查询数据至服务端
+			//接收服务端回传的查询结果
+			AllTrainAndStationInfo trainAndStationInfos = new AllTrainAndStationInfo();
+			TrainAndStationInfo trainAndStationInfo = new TrainAndStationInfo();
+			trainAndStationInfo.TrainNumber = 103;
+			trainAndStationInfo.StationNumber = 1151;
+			trainAndStationInfo.ArriveTime = DateTime.Now.AddHours(1);
+			trainAndStationInfo.LeaveTime = DateTime.Now.AddHours(1.3);
+			trainAndStationInfo.RemainSeats = 19;
+			trainAndStationInfos.Add(trainAndStationInfo);
+
+			trainAndStationInfo.TrainNumber = 104;
+			trainAndStationInfo.StationNumber = 1151;
+			trainAndStationInfo.ArriveTime = DateTime.Now.AddHours(1);
+			trainAndStationInfo.LeaveTime = DateTime.Now.AddHours(1.3);
+			trainAndStationInfo.RemainSeats = 19;
+			trainAndStationInfos.Add(trainAndStationInfo);
+
+			return trainAndStationInfos;
+
+		}
+		*/
 	}
 }
