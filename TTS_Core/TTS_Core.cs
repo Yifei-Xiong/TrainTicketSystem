b79e187663f4724114cbeb62541026b30141046c
huanghaicheng1024 <huang-haicheng@qq.com>
2020-08-18 11:41:04 +0800

    将数据包类型、查询类别数据结构更改为枚举类型

--- TTS_Core/TTS_Core.cs
+++ TTS_Core/TTS_Core.cs
@@ -9,8 +9,34 @@ using System.Threading.Tasks;
 
 namespace TTS_Core
 {
-	//数据包类，Type=0
-	[Serializable]
+    //数据包类型的枚举
+    public enum MESSAGETYPE
+    {
+        K_DATA_PACKAGE,  //数据包类
+        K_LOGIN_DATA_PACKAGE,  //登入数据包类
+        K_REGISTER_DATA_PACKAGE,  //注册数据包类
+        K_QUERY_DATA_PACKAGE,  //查询数据包类
+        K_TICKETQUERY_DATA_PACKAGE  //客户端向服务端发送的查询条件类
+    }
+
+    //查询类别的枚举
+    public enum QUERYTYPE
+    {
+        K_DEPARTURE_STATION,  // 出发站点查询
+        K_ARRIVAL_STATION,  //到达站点查询
+        K_USER_ORDER,  //用户订单查询
+        K_LINE,  //线路查询
+        K_TRAIN,  //列车查询
+        K_STATION,  //车站查询
+        K_STATION_LINE,  //车站-线路查询
+        K_TRAIN_STATION,  //列车-车站查询
+        K_TRAIN_PASSENGERS, //列车乘客查询
+        K_TICKET_PRICE  //车票价格查询
+    }
+
+
+    //数据包类，Type=0
+    [Serializable]
 	public abstract class DataPackage {
 		public DataPackage() {
 			sendTime = DateTime.Now;
@@ -30,7 +56,7 @@ namespace TTS_Core
 		public DateTime sendTime { get; set; } //消息的发送时间
 		public string Sender { get; set; }
 		public string Receiver { get; set; }
-		public int MessageType = 0; //数据包类Type为0
+		public MESSAGETYPE MessageType = MESSAGETYPE.K_DATA_PACKAGE; //数据包类Type为0
 	}
 
 	//登入数据包类，Type=1
@@ -51,7 +77,7 @@ namespace TTS_Core
 			}
 		} //构造函数 字节数组转化为数据包
 		public LoginDataPackage(string sender, string receiver, string userID, string password) : base(sender,receiver) {
-			MessageType = 1;
+			MessageType = MESSAGETYPE.K_LOGIN_DATA_PACKAGE;
 			this.UserID = userID;
 			this.Password = password;
 		} //构造函数 接受发送者,接收者字符串,登录用户名与密码
@@ -77,7 +103,7 @@ namespace TTS_Core
 			}
 		} //构造函数 字节数组转化为数据包
 		public RegisterDataPackage(string sender, string receiver, string userID, string password) : base(sender, receiver) {
-			MessageType = 2;
+			MessageType = MESSAGETYPE.K_REGISTER_DATA_PACKAGE;
 			this.UserID = userID;
 			this.Password = password;
 		} //构造函数 接受发送者,接收者字符串,注册用户名与注册密码
@@ -101,11 +127,11 @@ namespace TTS_Core
 				}
 			}
 		} //构造函数 字节数组转化为数据包
-		public QueryDataPackage(string sender, string receiver, int QueryType) : base(sender, receiver) {
-			MessageType = 11;
+		public QueryDataPackage(string sender, string receiver, QUERYTYPE QueryType) : base(sender, receiver) {
+			MessageType = MESSAGETYPE.K_QUERY_DATA_PACKAGE;
 			this.QueryType = QueryType;
 		} //构造函数 接受发送者,接收者字符串,注册用户名与注册密码
-		public int QueryType { get; set; } //查询类别
+		public QUERYTYPE QueryType { get; set; } //查询类别
 		// 1为出发站点查询，2为到达站点查询，3为用户订单查询
 		// 11为线路查询，12为列车查询，13为车站查询，14为车站-线路查询，15为列车-车站查询
 		// 16为列车乘客查询，17为车票价格查询
@@ -132,7 +158,7 @@ namespace TTS_Core
 		} //构造函数 字节数组转化为数据包
 		public TicketQueryDataPackage(string sender, string receiver, int EnterStationNumber,
 			int LeaveStationNumber, DateTime StartTime, DateTime EndTime) : base(sender, receiver) {
-			MessageType = 12;
+			MessageType = MESSAGETYPE.K_TICKETQUERY_DATA_PACKAGE;
 			this.EnterStationNumber = EnterStationNumber;
 			this.LeaveStationNumber = LeaveStationNumber;
 			this.StartTime = StartTime;
